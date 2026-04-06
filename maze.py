170282c362071a5b37f33f1bc6337ac1feae700c
loulwa-T <loulwa.tabannaj@gmail.com>
2026-04-06 19:56:58 +0200

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,21 +1,23 @@
 import turtle
 pen= turtle.Turtle()
 screen=turtle.Screen()
+pen.up()
 list=["xxxxxxxxxxxxxxx",
      "xx xx xx xx xx ",
-     "x     x      x ",
-     "x x x x x  x x ",
+     "x     x      xx",
+     "x x x x x  x xx",
      "x             x",
      "xx  xx  xx   xx",
-     "x  x  x  x  x  ",
-     "  xx        xxx",
+     "x  x  x  x  x x",
+     "x xx        xxx",
      "x  xx      xx x",
-     "         x  x  ",
-     "xxx xxx xx xx x",
-     "xx    xx  x x  ",
+     "x        x  x x",
+     "xxx xxx xx  x x",
+     "xx    xx  x   x",
      "xxxx   x     xx",
-     "x xx x x x x x ",
+     "x xx x x x x xx",
      "xxxxxxxxxxxxxxx"]
+tile_list=[]
 for y in range (len(list)):
     for x in range (len(list[y])):
      tilex= -288+(x*24)
@@ -28,14 +30,44 @@ for y in range (len(list)):
       tile.up()
       tile.speed(0)
       tile.goto(tilex,tiley)
+      tile_list.append(tile)
+def validmove(x,y):
+     for tile in tile_list:
+        if x==tile.xcor() and y==tile.ycor():
+             return False
+     return True
 def right():
-        x=pen.cor()
+        x=pen.xcor()
         x+=24
         y=pen.ycor()
-        pen.goto(x,y)
+        if validmove(x,y)==True:
+         pen.goto(x,y)
 screen.listen()
 screen.onkey(right,"Right")
+def left():
+        x=pen.xcor()
+        x-=24
+        y=pen.ycor()
+        if validmove(x,y)==True:
+         pen.goto(x,y)
+screen.listen()
+screen.onkey(left,"Left")
+def up():
+        y=pen.ycor()
+        y+=24
+        x=pen.xcor()
+        if validmove(x,y)==True:
+         pen.goto(x,y)
+screen.listen()
+screen.onkey(up,"Up")
+def Down():
+        y=pen.ycor()
+        y-=24
+        x=pen.xcor()
+        if validmove(x,y)==True:
+         pen.goto(x,y)
+screen.listen()
+screen.onkey(Down,"Down")
 turtle.done()
-
 
 
