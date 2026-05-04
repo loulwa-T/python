4ff4d361467459b31dd347b00466886636fa4ada
loulwa-T <loulwa.tabannaj@gmail.com>
2026-05-04 20:01:34 +0200

    Add files via upload

--- balloon.py
+++ balloon.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+turtle.colormode(255)
 screen=turtle.Screen()
 food=turtle.Turtle()
 food.shape("triangle")
@@ -17,11 +18,27 @@ def left():
     y=food.ycor()
     food.goto(x,y)
 screen.onkey(left,"Left")
-balloon=turtle.Turtle()
-balloon.shape("circle")
-balloon.up()
-balloon.left(90)
-balloon.goto(random.randint(-250,250),250)
+list=[]
+def bal():
+ balloon=turtle.Turtle()
+ balloon.speed(0)
+ balloon.hideturtle()
+ balloon.shape("circle")
+ balloon.color(random.randint(1,255),random.randint(1,255),random.randint(1,255))
+ balloon.up()
+ balloon.left(90)
+ balloon.goto(random.randint(-250,250),250)
+ balloon.showturtle()
+ list.append(balloon) 
 while True:
- balloon.backward(250)
-
+   t=random.randint(1,25)
+   if t==9:
+    bal()
+   for balloon in list:
+    balloon.backward(5)
+    if balloon.distance (food)<15:
+     list.remove(balloon)
+     balloon.hideturtle()
+    elif balloon.ycor()<-250:
+     balloon.hideturtle()
+     list.remove(balloon)
