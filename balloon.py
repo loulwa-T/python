00db2bbe68d2bd28dc5433355af050e8510dfb00
loulwa-T <loulwa.tabannaj@gmail.com>
2026-05-11 19:58:34 +0200

    Add files via upload

--- balloon.py
+++ balloon.py
@@ -10,15 +10,18 @@ food.left(90)
 def right():
     x=food.xcor()+10
     y=food.ycor()
-    food.goto(x,y)
+    if x<250:
+     food.goto(x,y)
 screen.listen()
 screen.onkey(right,"Right")
 def left():
     x=food.xcor()-10
     y=food.ycor()
-    food.goto(x,y)
+    if x>-250:
+     food.goto(x,y)
 screen.onkey(left,"Left")
 list=[]
+badlist=[]
 def bal():
  balloon=turtle.Turtle()
  balloon.speed(0)
@@ -29,8 +32,27 @@ def bal():
  balloon.left(90)
  balloon.goto(random.randint(-250,250),250)
  balloon.showturtle()
- list.append(balloon) 
-while True:
+ list.append(balloon)
+
+score=0
+sp=turtle.Turtle()
+
+
+def badbal():
+ badballoon=turtle.Turtle()
+ badballoon.speed(0)
+ badballoon.hideturtle()
+ badballoon.shape("circle")
+ badballoon.pencolor("red")
+ badballoon.fillcolor("black")
+ badballoon.up()
+ badballoon.left(90)
+ badballoon.goto(random.randint(-250,250),250)
+ badballoon.showturtle()
+ badlist.append(badballoon)
+
+gameover=True
+while gameover:
    t=random.randint(1,25)
    if t==9:
     bal()
@@ -39,6 +61,20 @@ while True:
     if balloon.distance (food)<15:
      list.remove(balloon)
      balloon.hideturtle()
+     score+1
+     sp.write(score)
     elif balloon.ycor()<-250:
      balloon.hideturtle()
      list.remove(balloon)
+   ti=random.randint(1,250)
+   if ti==2:
+     badbal()
+   for badballoon in badlist:
+     badballoon.backward(5)
+     if badballoon.distance (food)<15:
+      badballoon.write ("game over")
+      gameover=False
+     elif badballoon.ycor()<-250:
+      badballoon.hideturtle()
+      badlist.remove(badballoon)
+    
