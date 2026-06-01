1056ccbdfba859e5c7652b591271d1ee84f623ee
loulwa-T <loulwa.tabannaj@gmail.com>
2026-06-01 19:51:01 +0200

    Add files via upload

--- items.py
+++ items.py
@@ -1,11 +1,18 @@
-items={"bag":20,"shoes":10,"shirt":25,"jacket":30,"pant":5}
+products={"bag":20,"shoes":10,"shirt":25,"jacket":30,"pant":5}
 cart={}
 while True:
  product=input("what do you want to purchase")
  if product=="stop":
   break
 
- quantity=input("how many items would you like")
+ quantity=int(input("how many items would you like"))
  cart[product]=quantity
  
 print(cart)
+total=0
+for key, value in cart. items():
+ print(key,value,products[key])
+ amount=value*products[key]
+ print(amount)
+ tota=total+amount
+print(total)
