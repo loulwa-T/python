3f43ff3c7670a7322f846e141011708e25626959
loulwa-T <loulwa.tabannaj@gmail.com>
2026-06-23 18:15:05 +0200

    Add files via upload

---
+++ dubbel.py
@@ -0,0 +1,4 @@
+a=[[1,2],[3,4],[5,6]]
+for i in range (3):
+    for j in range (2):
+        print(a[i][j]*2)
---
+++ list+5.py
@@ -0,0 +1,4 @@
+l=[[1,2],[3,4],[5,6]]
+for i in range (3):
+    for j in range (2):
+        print(l[i][j]+5)
